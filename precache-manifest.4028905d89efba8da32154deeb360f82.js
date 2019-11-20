9f286c10c4764386d2e4ea422e6943874a0fe574
John Lopez <johnllopez616@gmail.com>
2019-11-20 10:18:29 -0800

    Updates

---
+++ precache-manifest.4028905d89efba8da32154deeb360f82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096d2a01054f6ffd3c32ce0d09be1517",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "9320eb1b7f637482e2c6",
+    "url": "/app/static/css/main.12bece96.chunk.css"
+  },
+  {
+    "revision": "67690c325b7a9636332b",
+    "url": "/app/static/js/2.80210623.chunk.js"
+  },
+  {
+    "revision": "9320eb1b7f637482e2c6",
+    "url": "/app/static/js/main.35890a87.chunk.js"
+  },
+  {
+    "revision": "98e10c0f37bf49337481",
+    "url": "/app/static/js/runtime~main.38779de5.js"
+  }
+]);
